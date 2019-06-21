a60b99e40862c8ea841d3345582f2d20bb4a757c
siroga <xez@example.com>
2019-06-21 15:24:47 +0000

    21june2019  end of the day

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export ANSIBLE_STDOUT_CALLBACK=debug
-ansible-playbook -i inventory/stage -l doc2 playbook.yml --tags=gerritdoc -vv
+ansible-playbook -i inventory/stage -l doc3 playbook.yml --tags=docker -vv
 #--tags=nexus
