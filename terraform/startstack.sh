bf04b551f76d46548407cc42cd9ed54c16cd72ea
siroga <xez@example.com>
2019-07-10 10:18:25 +0000

    80 port would be  able if selinux disable, add analog restart-always to container

--- terraform/startstack.sh
+++ terraform/startstack.sh
@@ -7,8 +7,7 @@ echo $EXT_IP_STACK >> stage
 
 export ANSIBLE_STDOUT_CALLBACK=debug
 #ansible-playbook -i stage -l local buildcontainers.yml --tags=postgrebuild -vvv
-#ansible-playbook -i stage -u devops --key-file ~/.ssh/gcloud_id_rsa runcontainers.yml --tags=dockerinstall
+ansible-playbook -i stage -u devops --key-file ~/.ssh/gcloud_id_rsa runcontainers.yml 
+# --tags=dockerinstall
 #ansible-playbook -i stage -u devops --key-file ~/.ssh/gcloud_id_rsa runcontainers.yml --skip-tags=dockerinstall
 #--tags=postgrerun -vvv 
-./startdocker1.sh
-./startstack2.sh
--- terraform/startstack2.sh
+++
@@ -1,6 +0,0 @@
-
-export ANSIBLE_STDOUT_CALLBACK=debug
-#ansible-playbook -i stage -l local buildcontainers.yml --tags=postgrebuild -vvv
-#ansible-playbook -i stage -u devops --key-file ~/.ssh/gcloud_id_rsa runcontainers.yml --tags=dockerinstall
-ansible-playbook -i stage -u devops --key-file ~/.ssh/gcloud_id_rsa runcontainers.yml --skip-tags=dockerinstall 
-#--tags=postgrerun -vvv
