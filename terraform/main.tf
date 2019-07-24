c0130994afcbcdcf8b0e9e1031b96102c43b7f62
srgnewcolorcheme <srg@nomail.xez.com>
2019-07-24 12:47:57 +0000

    added output for vms' ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,11 @@ provider "google" {
 }
 
 module "vm1" {
-  source        = "./modules/vms/"
-  instance_name = "${var.instance_name}"
+  source = "./modules/vms/"
+  #  instance_name = "${var.instance_name}"
+  instance_name = "vm1"
   image_size    = 11
 }
-
 
 
 module "vm2" {
@@ -27,3 +27,23 @@ module "vm2" {
 }
 
 
+#output "test"  {}
+
+###count    
+output "ext_ip_vm1" {
+  value = "${module.vm1.external_ip}"
+}
+output "int_ip_vm1" {
+  value = "${module.vm1.internal_ip}"
+}
+
+
+
+output "ext_ip_vm2" {
+  value = "${module.vm2.external_ip}"
+}
+
+output "int_ip_vm2" {
+  value = "${module.vm2.internal_ip}"
+}
+          
--- terraform/modules/vms/outputs.tf
+++ terraform/modules/vms/outputs.tf
@@ -5,9 +5,6 @@ output "external_ip" {
   value = "${google_compute_instance.vm.network_interface.0.access_config.0.nat_ip}"
   }
   
-
-/*
 output "internal_ip" {
-  value = "${module.vm1.google_compute_instance.vm.network_interface.0.network_ip}"
+  value = "${google_compute_instance.vm.network_interface.0.network_ip}"
 }
-*/
